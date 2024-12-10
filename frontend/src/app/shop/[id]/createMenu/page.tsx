6d772c9b9881d45550de0bca49479e8725409b2b
shimayuu <cnt19024@ict.nitech.ac.jp>
2024-12-10 16:32:39 +0900

    clientの仮ページです

--- frontend/src/app/shop/[id]/createMenu/page.tsx
+++ frontend/src/app/shop/[id]/createMenu/page.tsx
@@ -4,32 +4,32 @@ import type { Payment } from '@/components/dataTable/columns';
 import { columns } from '@/components/dataTable/columns';
 
 async function getData(): Promise<Payment[]> {
-    return [
-        {
-            id: '728ed52f',
-            price: 100,
-            name: '焼きそば',
-            time: 10,
-        },
-        {
-            id: '7ased52f',
-            price: 80,
-            name: '唐揚げ',
-            time: 5,
-        },
-    ];
+  return [
+    {
+      id: '728ed52f',
+      price: 100,
+      name: '焼きそば',
+      time: 10,
+    },
+    {
+      id: '7ased52f',
+      price: 80,
+      name: '唐揚げ',
+      time: 5,
+    },
+  ];
 }
 
 export default async function CreateMenuPage({ params }: { params: Promise<{ id: string }> }) {
-    const id = (await params).id;
-    const data = await getData();
+  const id = (await params).id;
+  const data = await getData();
 
-    return (
-        <div>
-            <CreateMenu id={id} />
-            <div className="container mx-auto py-10">
-                <DataTable columns={columns} data={data} />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <CreateMenu id={id} />
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={data} />
+      </div>
+    </div>
+  );
 }
