b148182ddc024ab14a7432e35d661cf4d4d5b21a
oldsheeep3 <js2oyb@gmail.com>
2024-12-16 10:53:23 +0000

    取扱説明書（内容未完成。）

---
+++ frontend/src/components/howtouse/HWTitle.tsx
@@ -0,0 +1,14 @@
+'use-client'
+
+import React, { ReactNode } from 'react';
+interface HWTitleProps {
+  no:number
+  children: React.ReactNode;
+}
+export default function HWTitle({no,children}:HWTitleProps){
+  const noS = no.toString();
+  const h2ClassName = `before:content-[attr(attr-cont)] before:mx-3 before:w-4 before:inline-block py-2 my-2 text-xl font-bold border-l-4 border-b-2 border-black`;
+  return(
+    <h2 attr-cont={no} className={h2ClassName}>{children}</h2>
+  )
+}
